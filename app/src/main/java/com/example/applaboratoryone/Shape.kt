c22942420b62768d2812613e5643756ab59c4160
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-10-02 20:35:51 +0300

    Added code to sealed "Shape" class

--- app/src/main/java/com/example/applaboratoryone/Shape.kt
+++ app/src/main/java/com/example/applaboratoryone/Shape.kt
@@ -1,5 +1,18 @@
 package com.example.applaboratoryone
 
 sealed class Shape {
+    abstract val type: ShapeType
 
+    data class Rectangle(val width: Int, val height: Int) : Shape() {
+        override val type = ShapeType.Rectangle
+    }
+
+    data class Oval(val majorAxis: Int, val minorAxis: Int) : Shape() {
+        override val type = ShapeType.Oval
+    }
+
+    data class Line(val length: Int) : Shape() {
+        override val type = ShapeType.Line
+    }
 }
+
