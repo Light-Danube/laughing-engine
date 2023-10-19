ec2e865ba2357d4c666ed47bdc0f1ab0ac89b090
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-10-20 01:26:17 +0300

    Testing Button Click

--- _LaboratoryWorks/_AndroidProject#1/app/src/main/java/com/example/hellokittyapp/MainActivity.kt
+++ _LaboratoryWorks/_AndroidProject#1/app/src/main/java/com/example/hellokittyapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.hellokittyapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import android.widget.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,6 +16,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             setContentView(R.layout.activity_main)
+        }
+
+        val detailsButton = findViewById<Button>(R.id.animalDetails1)
+
+        detailsButton.setOnClickListener {
+            // Define what happens when the button is clicked.
+            // For example, start a new activity.
+            println("Test")
         }
     }
 }
