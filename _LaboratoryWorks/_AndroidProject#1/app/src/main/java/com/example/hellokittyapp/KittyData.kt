2c959e3c486fed711ae836f07ca515e80589c340
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-10-23 13:37:43 +0300

    Added "Return" Functionality.

--- _LaboratoryWorks/_AndroidProject#1/app/src/main/java/com/example/hellokittyapp/KittyData.kt
+++ _LaboratoryWorks/_AndroidProject#1/app/src/main/java/com/example/hellokittyapp/KittyData.kt
@@ -1,11 +1,27 @@
 package com.example.hellokittyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class KittyData : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kitty_data)
+
+        val returnButton = findViewById<Button>(R.id.returnButton)
+
+        //val animalData = intent.getParcelableExtra<Animal>("Animal")
+
+        returnButton.setOnClickListener{
+            openSecondActivity()
+        }
+    }
+
+    private fun openSecondActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        //intent.getParcelableExtra<Animal>("Animal")
+        startActivity(intent)
     }
 }
