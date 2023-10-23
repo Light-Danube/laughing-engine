b3e33c372300b3e219e6dc6340867863a1cce842
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-10-23 12:14:21 +0300

    Added second activity

---
+++ _LaboratoryWorks/_AndroidProject#1/app/src/main/java/com/example/hellokittyapp/KittyData.kt
@@ -0,0 +1,11 @@
+package com.example.hellokittyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KittyData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kitty_data)
+    }
+}
