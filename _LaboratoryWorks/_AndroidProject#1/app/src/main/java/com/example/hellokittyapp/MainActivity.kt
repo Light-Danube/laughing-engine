a4fb19fa20f2249e01b427d5ff642502d2e27959
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-10-23 20:56:04 +0300

    Working over app design

--- _LaboratoryWorks/_AndroidProject#1/app/src/main/java/com/example/hellokittyapp/MainActivity.kt
+++ _LaboratoryWorks/_AndroidProject#1/app/src/main/java/com/example/hellokittyapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.hellokittyapp
 
+import android.app.Dialog
 import android.os.Bundle
 import android.content.Intent
+import android.view.MenuItem
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.widget.PopupMenu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +34,10 @@ class MainActivity : AppCompatActivity() {
         detailsButton3.setOnClickListener {
             openSecondActivity(animal3)
         }
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
     }
 
     private fun openSecondActivity(data: Animal) {
@@ -37,4 +45,54 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("Animal", data)
         startActivity(intent)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_about_creator -> {
+                showAboutCreatorPopup()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showAboutCreatorPopup() {
+        val popupMenu = PopupMenu(this, findViewById(R.id.toolbar))
+        popupMenu.menuInflater.inflate(R.menu.menu_main, popupMenu.menu)
+
+        val aboutCreatorItem = popupMenu.menu.findItem(R.id.action_about_creator)
+        aboutCreatorItem.setOnMenuItemClickListener {
+            /// Create a custom dialog
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.about_creator_dialog)
+
+            // Customize the dialog's appearance (optional)
+            dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+            dialog.window?.setLayout(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT
+            )
+
+            // Find and customize elements in the dialog layout
+            val creatorName = dialog.findViewById<TextView>(R.id.creatorName)
+            val creatorDescription = dialog.findViewById<TextView>(R.id.creatorDescription)
+
+            creatorName.text = "John Doe" // Replace with the creator's name
+            creatorDescription.text = "A passionate app developer and tech enthusiast."
+
+            // Handle the close button click
+            val closeButton = dialog.findViewById<Button>(R.id.closeButton)
+            closeButton.setOnClickListener {
+                dialog.dismiss() // Close the dialog
+            }
+
+            dialog.show() // Show the dialog
+
+            true
+        }
+
+        popupMenu.show()
+    }
+
+
 }
