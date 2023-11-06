0e60769b58f9f4b7650328d9a7d2c40d04fd0b22
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-06 12:59:04 +0200

    Working over navigation.

--- _LaboratoryWorks/MyFragments/app/src/main/java/com/example/myfragments/MainActivity.kt
+++ _LaboratoryWorks/MyFragments/app/src/main/java/com/example/myfragments/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.myfragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find nav host fragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_graph)
+                as NavHostFragment
+
+        // Get nav controller
+        val navController = navHostFragment.navController
     }
 }
