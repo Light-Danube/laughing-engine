963e0e637fab368d52c973fab6b6ae9f983f6332
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-18 23:39:41 +0200

    Added Second Fragment

---
+++ _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/presentation/WeatherFragment.kt
@@ -0,0 +1,17 @@
+package com.example.trueweather.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.trueweather.R
+class WeatherFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather, container, false)
+    }
+}
