0fa848092c498cec8f3b1f245d7060e28afdf6c0
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-27 00:19:12 +0200

    Working over navigation & design

--- _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/presentation/MainActivity.kt
+++ _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/presentation/MainActivity.kt
@@ -8,5 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize Navigation
+        //val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        // Set default destination
+        //navController.navigate(R.id.fragment_weather)
     }
 }
