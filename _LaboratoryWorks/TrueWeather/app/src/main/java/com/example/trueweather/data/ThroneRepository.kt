324c35bda7db18e64388bea362c2a3d2b3fba6cf
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-28 01:41:19 +0200

    Update ThroneRepository.kt

--- _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/data/ThroneRepository.kt
+++ _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/data/ThroneRepository.kt
@@ -2,11 +2,12 @@ package com.example.trueweather.data
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.flow.map
 import com.example.trueweather.data.remote.ThroneAPIInterface
 import com.example.trueweather.data.remote.model.ThroneHero
 
 class ThroneRepository(private val api: ThroneAPIInterface) {
-    fun getCharacters(): LiveData<List<ThroneHero>> {
+    suspend fun getCharacters(): LiveData<List<ThroneHero>> {
         return api.getCharacters().execute().body()?.let {
             MutableLiveData<List<ThroneHero>>().apply {
                 value = it.map { it.toThroneHero() }
@@ -16,7 +17,7 @@ class ThroneRepository(private val api: ThroneAPIInterface) {
         }
     }
 
-    fun getCharacter(id: Int): LiveData<ThroneHero> {
+    suspend fun getCharacter(id: Int): LiveData<ThroneHero> {
         return api.getCharacter(id).execute().body()?.let {
             MutableLiveData<ThroneHero>().apply {
                 value = it.toThroneHero()
