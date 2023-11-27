8d4c3686cb3a1618843b319009de4ff3a13edc45
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-28 00:44:29 +0200

    Update ThroneRepository.kt

--- _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/data/ThroneRepository.kt
+++ _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/data/ThroneRepository.kt
@@ -1,5 +1,7 @@
 package com.example.trueweather.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.trueweather.data.remote.ThroneAPIFactory
 import com.example.trueweather.data.remote.ThroneAPIInterface
 import com.example.trueweather.data.remote.model.ThroneHero
@@ -10,7 +12,7 @@ class ThroneRepository : BaseRepository() {
         ThroneAPIFactory().create()
     }
 
-    fun getCharacters(): LiveData<List<ThroneHero>> {
+    suspend fun getCharacters(): LiveData<List<ThroneHero>> {
         return api.getCharacters().execute().body()?.let {
             MutableLiveData<List<ThroneHero>>().apply {
                 value = it
@@ -20,7 +22,7 @@ class ThroneRepository : BaseRepository() {
         }
     }
 
-    fun getCharacter(id: Int): LiveData<ThroneHero> {
+    suspend fun getCharacter(id: Int): LiveData<ThroneHero> {
         return api.getCharacter(id).execute().body()?.let {
             MutableLiveData<ThroneHero>().apply {
                 value = it
