1a1bcb0903a05f36b0e864a796505f6ff9c25e38
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-27 22:26:25 +0200

    Update ThroneAPIInterface.kt

--- _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/data/remote/ThroneAPIInterface.kt
+++ _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/data/remote/ThroneAPIInterface.kt
@@ -1,13 +1,14 @@
 package com.example.trueweather.data.remote
 
-import okhttp3.Response
+import com.example.trueweather.data.remote.model.ThroneHero
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ThroneAPIInterface {
     @GET("characters")
-    suspend fun getCharacters(): Response<List<Character>>
+    suspend fun getCharacters(): Response<List<ThroneHero>>
 
     @GET("characters/{id}")
-    suspend fun getCharacter(@Path("id") id: Int): Response<Character>
+    suspend fun getCharacter(@Path("id") id: Int): Response<ThroneHero>
 }
