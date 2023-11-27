24ed4de6bde3293cbf9870ebcf0da9c32b0ade2a
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-28 00:57:33 +0200

    Update ThroneRepository.kt

--- _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/data/ThroneRepository.kt
+++ _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/data/ThroneRepository.kt
@@ -2,30 +2,45 @@ package com.example.trueweather.data
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.example.trueweather.data.remote.ThroneAPIFactory
 import com.example.trueweather.data.remote.ThroneAPIInterface
 import com.example.trueweather.data.remote.model.ThroneHero
 
 class ThronesRepository(private val api: ThroneAPIInterface) {
 
-    suspend fun getCharacters(): LiveData<List<ThroneHero>> {
+    fun getCharacters(): LiveData<List<ThroneHero>> {
         return api.getCharacters().execute().body()?.let {
             MutableLiveData<List<ThroneHero>>().apply {
-                value = it
+                value = it.map { it.toThroneHero() }
             }
         } ?: MutableLiveData<List<ThroneHero>>().apply {
             value = emptyList()
         }
     }
 
-    suspend fun getCharacter(id: Int): LiveData<ThroneHero> {
+    fun getCharacter(id: Int): LiveData<ThroneHero> {
         return api.getCharacter(id).execute().body()?.let {
             MutableLiveData<ThroneHero>().apply {
-                value = it
+                value = it.toThroneHero()
             }
         } ?: MutableLiveData<ThroneHero>().apply {
             value = ThroneHero()
         }
     }
+
+    private fun ThroneAPIResponse<ThroneHero>.toThroneHero(): ThroneHero {
+        return ThroneHero(
+            id = id,
+            firstName = firstName,
+            lastName = lastName,
+            fullName = fullName,
+            title = title,
+            family = family,
+            image = image,
+            imgURL = imgURL
+        )
+    }
 }
+
+
+
 
