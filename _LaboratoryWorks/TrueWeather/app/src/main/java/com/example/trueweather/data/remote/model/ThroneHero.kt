19fa654b2fb467107c20284efdc39ada9312299f
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-28 21:28:50 +0200

    Update ThroneHero.kt

--- _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/data/remote/model/ThroneHero.kt
+++ _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/data/remote/model/ThroneHero.kt
@@ -1,5 +1,8 @@
 package com.example.trueweather.data.remote.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class ThroneHero(
     val id: Int,
     val firstName: String?,
@@ -9,4 +12,40 @@ data class ThroneHero(
     val family: String?,
     val image: String?,
     val imgURL: String?
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(firstName)
+        parcel.writeString(lastName)
+        parcel.writeString(fullName)
+        parcel.writeString(title)
+        parcel.writeString(family)
+        parcel.writeString(image)
+        parcel.writeString(imgURL)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ThroneHero> {
+        override fun createFromParcel(parcel: Parcel): ThroneHero {
+            return ThroneHero(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ThroneHero?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
