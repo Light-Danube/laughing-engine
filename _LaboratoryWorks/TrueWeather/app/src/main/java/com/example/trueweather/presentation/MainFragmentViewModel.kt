0099bbf768a208f5d062089b1daff73cd9f1807c
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-28 20:34:53 +0200

    Update MainFragmentViewModel.kt

--- _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/presentation/MainFragmentViewModel.kt
+++ _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/presentation/MainFragmentViewModel.kt
@@ -3,8 +3,19 @@ package com.example.trueweather.presentation
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.trueweather.data.ThroneRepository
+import com.example.trueweather.data.remote.model.ThroneHero
 import kotlinx.coroutines.coroutineScope
 
 class MainFragmentViewModel(private val repository: ThroneRepository): ViewModel() {
-    val id: MutableLiveData<List<Int>> = MutableLiveData(emptyList())
+    // Live data for loaded characters
+    val characters = MutableLiveData<List<ThroneHero>>()
+
+    // Total number of characters
+    var totalCount = 0
+
+    fun loadCharacters() {
+        // Get characters from repository
+        characters.value = repository.getCharacters()
+        totalCount = characters.value!!.size // Update total count
+    }
 }
