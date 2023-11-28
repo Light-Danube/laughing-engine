f1df2d69acb0f681987d4f3f8715923522f74f97
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-28 22:50:46 +0200

    Update build.gradle.kts

--- _LaboratoryWorks/TrueWeather/app/build.gradle.kts
+++ _LaboratoryWorks/TrueWeather/app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation ("com.github.bumptech.glide:glide:4.14.2")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
