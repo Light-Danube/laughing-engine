b8e19dfa89f9ac8af1c4ec5a2e150e870fd36b9a
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-28 18:49:10 +0200

    Update MainFragmentViewModel.kt

--- _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/presentation/MainFragmentViewModel.kt
+++ _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/presentation/MainFragmentViewModel.kt
@@ -1,6 +1,18 @@
 package com.example.trueweather.presentation
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.trueweather.data.ThroneRepository
 
-class MainFragmentViewModel: ViewModel() {
+class MainFragmentViewModel(private val repository: ThroneRepository): ViewModel() {
+
+    val id: MutableLiveData<List<Int>> = MutableLiveData(emptyList())
+
+
+    suspend fun loadIdFromApi() {
+        // Використання ThroneUseCase для отримання ідентифікаторів персонажів з API
+        val ids = repository.getCharacters().body()?.map { it.id } ?: emptyList()
+        // Присвоєння отриманого значення id до LiveData
+        this.id.value = ids
+    }
 }
