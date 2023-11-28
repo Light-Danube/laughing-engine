62ddb165517aa94c892ba58ae398a880312b6a7f
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-28 13:38:48 +0200

    Update WeatherUseCase.kt

--- _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/domain/WeatherUseCase.kt
+++ _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/domain/WeatherUseCase.kt
@@ -1,4 +1,15 @@
 package com.example.trueweather.domain
 
-class WeatherUseCase {
+import androidx.lifecycle.LiveData
+import com.example.trueweather.data.ThroneRepository
+import com.example.trueweather.data.remote.model.ThroneHero
+
+class WeatherUseCase (private val repository: ThroneRepository){
+    suspend fun getCharacters(): LiveData<List<ThroneHero>> {
+        return repository.getCharacters()
+    }
+
+    suspend fun getCharacter(id: Int): LiveData<ThroneHero> {
+        return repository.getCharacter(id)
+    }
 }
