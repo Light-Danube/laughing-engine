3728ce0bd357aca0bc5c95a509ba0c88ab2f6847
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-28 21:04:50 +0200

    Update ThroneHero.kt

--- _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/data/remote/model/ThroneHero.kt
+++ _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/data/remote/model/ThroneHero.kt
@@ -2,11 +2,11 @@ package com.example.trueweather.data.remote.model
 
 data class ThroneHero(
     val id: Int,
-    val firstName: String,
-    val lastName: String,
-    val fullName: String,
-    val title: String,
-    val family: String,
-    val image: String,
-    val imgURL: String
+    val firstName: String?,
+    val lastName: String?,
+    val fullName: String?,
+    val title: String?,
+    val family: String?,
+    val image: String?,
+    val imgURL: String?
 )
