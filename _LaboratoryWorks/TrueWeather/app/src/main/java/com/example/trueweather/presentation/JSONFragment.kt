998ec70f5a69e409bda67f53d6490ded01114a1b
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-28 21:27:17 +0200

    Tweaking JSON loading

--- _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/presentation/JSONFragment.kt
+++ _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/presentation/JSONFragment.kt
@@ -1,23 +1,34 @@
 package com.example.trueweather.presentation
 
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.trueweather.R
+import com.example.trueweather.data.remote.model.ThroneHero
 
 
+@Suppress("DEPRECATION")
 class JSONFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_secunda, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val character = arguments?.getParcelable<Parcelable>("character") as? ThroneHero
+
+        // Assuming you have a TextView with ID "jsonTextView" in your layout
+        val jsonTextView = view.findViewById<TextView>(R.id.jsonAppText)
+        jsonTextView.text = character?.toString() ?: "Character not found"
     }
 }
