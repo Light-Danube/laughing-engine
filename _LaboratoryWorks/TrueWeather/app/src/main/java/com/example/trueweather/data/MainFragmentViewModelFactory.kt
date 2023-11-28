30fdee870d42ec8bf638b2619dc5dba8cd1211d4
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-28 20:56:39 +0200

    Create MainFragmentViewModelFactory.kt

---
+++ _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/data/MainFragmentViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.example.trueweather.data
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.trueweather.presentation.MainFragmentViewModel
+
+class MainFragmentViewModelFactory(private val repository: ThroneRepository) : ViewModelProvider.Factory {
+    fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainFragmentViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MainFragmentViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
