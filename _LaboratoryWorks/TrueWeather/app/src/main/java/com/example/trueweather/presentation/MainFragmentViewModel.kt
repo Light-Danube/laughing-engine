f8221e2ec247d19a9a4ae8bfe9942f35ba3ee749
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-11-28 20:10:26 +0200

    Update MainFragmentViewModel.kt

--- _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/presentation/MainFragmentViewModel.kt
+++ _LaboratoryWorks/TrueWeather/app/src/main/java/com/example/trueweather/presentation/MainFragmentViewModel.kt
@@ -3,15 +3,19 @@ package com.example.trueweather.presentation
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.trueweather.data.ThroneRepository
+import kotlinx.coroutines.coroutineScope
 
 class MainFragmentViewModel(private val repository: ThroneRepository): ViewModel() {
     val id: MutableLiveData<Int?> = MutableLiveData(null)
 
-    suspend fun loadIdFromApi() {
-        // Використання ThroneUseCase для отримання ідентифікатора персонажа з API
-        val characters = repository.getCharacters().body() ?: return
+    suspend fun loadIdFromApi() = coroutineScope {
+        // Use repository.getCharacters() to retrieve all characters from the API
+        val characters = repository.getCharacters().await()
+
+        // Extract the first ID from the list of characters
         val id = characters.firstOrNull()?.id
-        // Присвоєння отриманого значення id до LiveData
-        id?.let { this.id.value = it }
+
+        // Update the LiveData with the extracted ID
+        id?.let { this.id.postValue(it) }
     }
 }
